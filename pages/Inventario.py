8b160ff498cabf68f1c9f9d5bc835901fc83198a
DAAR-W3B <a01287191@tec.mx>
2024-06-15 16:30:59 -0600

    test2

--- pages/Inventario.py
+++ pages/Inventario.py
@@ -1,2 +1,11 @@
 import streamlit as st
+import numpy as np
+import pandas as pt
+
+BaseDatos_PreciosMexico = "BaseDatos_PreciosMexico.xlsx"
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+
+
 st.title("Tu inventario Actual")
