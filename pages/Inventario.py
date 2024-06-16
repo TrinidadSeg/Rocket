c1a19fcf618a35058bf6af54609c27cac271921d
Trinidadseg <tsegovialtri@gmail.com>
2024-06-15 23:02:51 -0500

    ji

--- pages/Inventario.py
+++ pages/Inventario.py
@@ -11,31 +11,11 @@ def custom_header(User, background_color):
     st.markdown(html_code, unsafe_allow_html=True)
 custom_header("Trini", "fffff")
 
-
-st.write("Inventario de Productos")
-image_path = "assets/Cocacola_01.png"
-st.image(image_path, caption="Your Image Caption", width=300)
-
-
 # read by default 1st sheet of an excel file
 df = pd.read_excel('pages/BaseDatos_PreciosMexico.xlsx')
-
-
-st.dataframe(df)
-
     # Inputs for row index and column name
 row_index = st.number_input("Enter row index", min_value=0, max_value=len(df)-1, step=1)
 column_name = st.text_input("Enter column name")
-
-    # Accessing a specific value
-if column_name:
-        try:
-            value = df.loc[row_index, column_name]
-            st.write(f"The value at row {row_index}, column '{column_name}' is: {value}")
-        except KeyError:
-            st.write(f"Column '{column_name}' does not exist in the DataFrame.")
-        except IndexError:
-            st.write(f"Row {row_index} does not exist in the DataFrame.")
 list_of_products = []
 list_of_prices = []
 for counter in range(0,511):
@@ -43,26 +23,16 @@ for counter in range(0,511):
     list_of_prices.append(df.loc[counter, "Precio Final"])
     counter+=1
 
-st.write(f"{len(list_of_products)} items in inventory")
-
-#x = st.slider('x')  # 👈 this is a widget
-
-#st.write(x, 'squared is', x * x)
-
-#Start making the products available
 
 
 if 'cart' not in st.session_state:
     st.session_state.cart = []
 
 # List of products (name, price, description)
-products = [
-   
-]
-for nCounter in range(0,511):
-    {"id": nCounter, 'name': list_of_products[nCounter], 'price': list_of_prices[nCounter]},
+products = []
 
-    counter+=1
+for nCounter in range(0,511):
+    products.append({"id": nCounter, 'name': list_of_products[nCounter], 'price': list_of_prices[nCounter]})
 
 # Function to add a product to the cart
 def add_to_cart(product_id):
@@ -80,17 +50,18 @@ st.header("Products")
 for product in products:
     col1, col2, col3 = st.columns([3, 1, 1])
     with col1:
-        st.write(f"**{product['name']}** - ${product['price']:.2f}")
+        st.write(f"**{product['name']}** - ${product['price']}")
         st.write("hi")
     with col2:
         st.button("Add to Cart", key=product['id'], on_click=add_to_cart, args=(product['id'],))
+
 
 # Display shopping cart
 st.header("Shopping Cart")
 if st.session_state.cart:
     total = 0
     for item in st.session_state.cart:
-        st.write(f"{item['name']} - ${item['price']:.2f}")
+        st.write(f"{item['name']} - ${item['price']}")
         total += item['price']
     st.write(f"**Total: ${total:.2f}**")
 else:
